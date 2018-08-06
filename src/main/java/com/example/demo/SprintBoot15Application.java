3dc69c919d68580b4e5b69947adce3263f923290
tsigereda-affa <affa16.a@gmail.com>
2018-08-06 15:46:18 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/SprintBoot15Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBoot15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBoot15Application.class, args);
+	}
+}
